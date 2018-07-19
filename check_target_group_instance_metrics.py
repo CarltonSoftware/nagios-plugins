20e16a0d80af6671161555bbe0691cc07d89522f
Pete Lawrence <petejlawrence@icloud.com>
2018-07-19 14:50:13 +0100

    Added the ability to query for metrics of EBS volumes attached to instances in the target group

--- check_target_group_instance_metrics.py
+++ check_target_group_instance_metrics.py
@@ -8,40 +8,44 @@ import datetime
 
 elbv2 = boto3.client('elbv2')
 cloudwatch = boto3.client('cloudwatch')
+ec2 = boto3.client('ec2')
 
 
 # data acquisition
 
 class TGInstanceMetric(nagiosplugin.Resource):
 
-    def __init__(self, target_group_arn, metric_name, statistic, period):
+    def __init__(self, target_group_arn, metric_name, statistic, period, volume):
         self.target_group_arn = target_group_arn
         self.metric_name = metric_name
         self.period = period
         self.statistic = statistic
+        self.volume = volume
 
     # Perform the service check
     # Returns a nagiosplugin.Metric for each instance in the Target Group
     def probe(self):
         instances = self.get_instances_in_target_group(self.target_group_arn)
         results = []
-        for instance in instances:
-            dp = self.get_most_recent_data_point(instance, self.metric_name, self.statistic, self.period)
-            results.append(nagiosplugin.Metric(instance, dp, context=self.metric_name))
+
+        if self.volume:
+            volumes = self.get_volumes_for_instances(instances, self.volume)
+
+            for volume_id in volumes:
+                dps = self.get_volume_metric(volume_id, self.metric_name, self.statistic, self.period)
+                dp = self.get_most_recent_data_point(dps, self.statistic)
+                results.append(nagiosplugin.Metric(volume_id, dp, context=self.metric_name))
+
+        else:
+
+            for instance_id in instances:
+                dps = self.get_instance_metric(instance_id, self.metric_name, self.statistic, self.period)
+                dp = self.get_most_recent_data_point(dps, self.statistic)
+                results.append(nagiosplugin.Metric(instance_id, dp, context=self.metric_name))
 
         return results
 
-    def get_instances_in_asg(self, auto_scaling_group_name):
-
-        response = autoscaling.describe_auto_scaling_groups(
-            AutoScalingGroupNames = [auto_scaling_group_name]
-        )
-        instance_ids = []
-        for instance in response['AutoScalingGroups'][0]['Instances']:
-            instance_ids.append(instance['InstanceId'])
-
-        return instance_ids
-
+    # Returns a list of ids of instances in the supplied target group
     def get_instances_in_target_group(self, target_group_arn):
         response = elbv2.describe_target_health(
             TargetGroupArn = target_group_arn
@@ -52,11 +56,28 @@ class TGInstanceMetric(nagiosplugin.Resource):
 
         return instance_ids
 
-    def get_most_recent_data_point(self, instance_id, metric_name, statistic, period):
+
+    # Returns a list of volume_ids attached to the supplied instances with the supplied volume name
+    def get_volumes_for_instances(self, instance_ids, volume_name):
+        response = ec2.describe_instances(
+            InstanceIds = instance_ids
+        )
+
+        volume_ids = []
+        for instance in response['Reservations'][0]['Instances']:
+            for volume in instance['BlockDeviceMappings']:
+                print(volume['DeviceName'])
+                if volume['DeviceName'] == volume_name:
+                    volume_ids.append(volume['Ebs']['VolumeId'])
+
+        return volume_ids
+
+
+    # Returns trhe most recent data point from a list of data points
+    def get_most_recent_data_point(self, data_points, statistic):
         most_recent_timestamp = datetime.datetime(1970, 1, 1)
         most_recent_reading = ''
 
-        data_points = self.get_recent_data_points(instance_id, metric_name, statistic, period)
         for data_point in data_points:
             if (data_point['Timestamp'].replace(tzinfo=None) > most_recent_timestamp):
                 most_recent_timestamp = data_point['Timestamp'].replace(tzinfo=None)
@@ -64,7 +85,7 @@ class TGInstanceMetric(nagiosplugin.Resource):
 
         return most_recent_reading
 
-    def get_recent_data_points(self, instance_id, metric_name, statistic, period):
+    def get_instance_metric(self, instance_id, metric_name, statistic, period):
         response = cloudwatch.get_metric_statistics(
             Namespace = 'AWS/EC2',
             MetricName = metric_name,
@@ -72,6 +93,23 @@ class TGInstanceMetric(nagiosplugin.Resource):
                 {
                     'Name': 'InstanceId',
                     'Value': instance_id
+                }
+            ],
+            StartTime = datetime.datetime.utcnow() - datetime.timedelta(seconds=600),
+            EndTime = datetime.datetime.utcnow(),
+            Period = period,
+            Statistics = [ statistic ]
+        )
+        return response['Datapoints']
+
+    def get_volume_metric(self, volume_id, metric_name, statistic, period):
+        response = cloudwatch.get_metric_statistics(
+            Namespace = 'AWS/EBS',
+            MetricName = metric_name,
+            Dimensions = [
+                {
+                    'Name': 'VolumeId',
+                    'Value': volume_id
                 }
             ],
             StartTime = datetime.datetime.utcnow() - datetime.timedelta(seconds=600),
@@ -102,12 +140,14 @@ def main():
     argp.add_argument('-p', '--period', metavar='PERIOD', default=60, type=int,
                       help='metric resolution in seconds')
     argp.add_argument('-s', '--statistic', metavar='Average/Minimum/etc', default='Average',
-                    help='statistic to monitor')
+                      help='statistic to monitor')
+    argp.add_argument('-v', '--volume', metavar='Volume Name', default=None,
+                      help='Volume to get metric of')
     args = argp.parse_args()
 
     # Perform the check
     check = nagiosplugin.Check(
-        TGInstanceMetric(args.targetgroup, args.metric, args.statistic, args.period),
+        TGInstanceMetric(args.targetgroup, args.metric, args.statistic, args.period, args.volume),
         nagiosplugin.ScalarContext(args.metric, args.warning, args.critical),
         TGInstanceMetricSummary()
     )
